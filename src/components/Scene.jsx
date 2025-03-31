4895c468ec4ab5d68099f3dcb3a664d10a9ca7b0
lufyDev <vishalpundhirofficial@gmail.com>
2025-03-31 15:00:11 +0530

    added environment map

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -10,6 +10,20 @@ const Scene = () => {
     // Scene setup
     const scene = new THREE.Scene()
 
+    //environment map
+    const cubeTextureLoader = new THREE.CubeTextureLoader()
+    const environmentMap = cubeTextureLoader.load([
+      './textures/Standard-Cube-Map/px.png', // positive x
+      './textures/Standard-Cube-Map/nx.png', // negative x
+      './textures/Standard-Cube-Map/py.png', // positive y
+      './textures/Standard-Cube-Map/ny.png', // negative y
+      './textures/Standard-Cube-Map/pz.png', // positive z
+      './textures/Standard-Cube-Map/nz.png'  // negative z
+    ])
+    // Set the scene's environment map
+    scene.environment = environmentMap
+    // Optional: Also use it as background
+    scene.background = environmentMap
     //Sizes
     const sizes = {
       width: window.innerWidth,
